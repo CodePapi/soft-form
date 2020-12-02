1ae65ef340d67bdec6fadb36065c8cb38b6c5167
Orim Dominic Adah <orimdominic@gmail.com>
2020-12-02 16:15:15 +0100

    Change Github link to that of repo

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ function Footer() {
   return (
     <div className="h-12 bg-gray-700">
       <ul className="flex flex-row items-center text-white justify-center h-full text-sm">
-        <li className=""><a  className="inline-block cursor-pointer px-2 font-mono hover:underline" href="https://github.com/sudo-kaizen" target="_blank" rel="noreferrer">Github:sudo-kaizen</a></li>
+        <li className=""><a  className="inline-block cursor-pointer px-2 font-mono hover:underline" href="https://github.com/sudo-kaizen/soft-form/tree/public" target="_blank" rel="noreferrer">Github:soft-form</a></li>
         <li className=""><a className="inline-block cursor-pointer px-2 font-mono hover:underline"  href="https:/twitter.com/sudo_kaizen" target="_blank" rel="noreferrer">Twitter:sudo_kaizen</a></li>
       </ul>
     </div>
